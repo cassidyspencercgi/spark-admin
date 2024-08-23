46f2d9d47f1daecc5fecf255ab189e2cfd9d08ca
Kurt <kurt.brewer@cgi.com>
2024-08-23 16:11:14 -0400

    fixed login css and added invalid login popup

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,9 @@ import { Component, inject, input, computed} from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { AssetService } from '../assets.service';
 import { Login } from '../login';
+import { MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
@@ -19,6 +21,7 @@ export class LoginComponent {
 
   assetService: AssetService = inject(AssetService);
   router: Router = inject(Router);
+  readonly dialog = inject(MatDialog);
 
   login_email: string = '';
   login_password: string = '';
@@ -39,7 +42,39 @@ export class LoginComponent {
       }).then(() => {
         this.router.navigate(['/home/assets']);
       }).catch(e => {
+        this.openDialog();
       console.log('WRONG LOGIN');
     });
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(InvalidLoginDialog, {
+      data: {},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+}
+
+@Component({
+  selector: 'invalid-login-dialog',
+  templateUrl: 'invalid-login-dialog.html',
+  standalone: true,
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatButtonModule
+  ],
+})
+
+export class InvalidLoginDialog {
+  readonly dialogRef = inject(MatDialogRef<InvalidLoginDialog>);
+
+  onOkClick(): void {
+    this.dialogRef.close();
   }
 }
