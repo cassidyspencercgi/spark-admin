84768acd5d980fd2c1e2867cb3fb3ecf93626d39
Spencer <cassidy.spencer@cgi.com>
2024-08-26 11:16:33 -0400

    waiting on get by name endpoints for category and type

--- src/app/assets.service.ts
+++ src/app/assets.service.ts
@@ -2,15 +2,18 @@ import { inject, Injectable } from "@angular/core";
 import { Asset } from "./asset";
 import { Login } from "./login";
 import { Router } from "@angular/router";
-
+import { PathVariables } from "./path.variables";
+import { Category } from "./category";
+import { AssetType } from "./asset.type";
 @Injectable({
     providedIn: 'root'
 })
 export class AssetService {
     baseurl = 'http://localhost:8000/autismapp'
     private _token : string = ''
-    adminUser : Login = {login_email: 'user', login_password: 'pass'}
     router: Router = inject(Router);
+    path: PathVariables = inject(PathVariables);
+
    
      constructor() {
         this._token = localStorage.getItem('authToken') || '';
@@ -24,9 +27,19 @@ export class AssetService {
     get token() {
         return this._token;
     }
+    /***********************Asset***********************/
+    async createAsset(newAsset: Asset) {
+        fetch(this.baseurl + this.path.ASSET, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newAsset),
+          })
+    }
 
     async getAssets() : Promise<Asset[]> {
-        const data = await fetch(this.baseurl + "/asset/v1/",
+        const data = await fetch(this.baseurl + this.path.ASSET,
             {
             method: 'GET',
             headers: {
@@ -39,13 +52,92 @@ export class AssetService {
         if (data.ok) {
             return data.json();
         } else {
-            this.router.navigate(['/login']);
+            this.router.navigate(['/login/']);
             throw new Error(`Error in get assets function`);
         }
     }
 
+    /***********************Category***********************/
+    async getCategories() : Promise<Category[]> {
+        const data = await fetch(this.baseurl + this.path.CATEGORY,
+            {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${this.token}`,
+                'Content-Type': 'application/json'
+            }
+            }
+        );
+        console.log(this.token);
+        if (data.ok) {
+            return data.json();
+        } else {
+            this.router.navigate(['/assets/']);
+            throw new Error(`Error in getting categories`);
+        }
+    }
+
+    async getCategoryIdbyName(name: string) : Promise<number> { ///TODOOOOO
+        const data = await fetch(this.baseurl + this.path.CATEGORY + '?category_name=' + name,
+            {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${this.token}`,
+                'Content-Type': 'application/json'
+            }
+            }
+        );
+        console.log(this.token);
+        if (data.ok) {
+            return data.json();
+        } else {
+            this.router.navigate(['/assets/']);
+            throw new Error(`Error in getting categories`);
+        }
+    }
+
+    /***********************Asset Type***********************/
+    async getTypes() : Promise<AssetType[]> {
+        const data = await fetch(this.baseurl + this.path.TYPE,
+            {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${this.token}`,
+                'Content-Type': 'application/json'
+            }
+            }
+        );
+        console.log(this.token);
+        if (data.ok) {
+            return data.json();
+        } else {
+            this.router.navigate(['/assets/']);
+            throw new Error(`Error in getting types`);
+        }
+    }
+
+    async getTypeIdbyName(name: string) : Promise<number> { ///TODOOOO
+        const data = await fetch(this.baseurl + this.path.CATEGORY + 'type' + name,
+            {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${this.token}`,
+                'Content-Type': 'application/json'
+            }
+            }
+        );
+        console.log(this.token);
+        if (data.ok) {
+            return data.json();
+        } else {
+            this.router.navigate(['/assets/']);
+            throw new Error(`Error in get type id by name`);
+        }
+    }
+
+    /***********************Auth***********************/
     async authorizeUser(adminUser : Login) : Promise<string> {
-        const data = await fetch(this.baseurl + "/adminUser/auth/v1/", {
+        const data = await fetch(this.baseurl + this.path.AUTH, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -58,25 +150,5 @@ export class AssetService {
             } else {
                 throw new Error(`Failed to fetch assets`);
             } 
-    }
-
-    async isUserAuth() : Promise<boolean> {
-        const data = await fetch(this.baseurl + "/asset/v1/",
-            {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${this.token}`,
-                'Content-Type': 'application/json'
-            }
-            }
-        );
-        console.log(this.token);
-        if (data.ok) {
-            console.log('true');
-            return true;
-        } else {
-            console.log('false');
-            return false;
-        }
     }
 }
--- src/app/manage-asset/create-asset/create-asset.component.ts
+++ src/app/manage-asset/create-asset/create-asset.component.ts
@@ -9,6 +9,8 @@ import { MatInputModule } from '@angular/material/input';
 import { AssetService } from '../../assets.service';
 import { ManageAssetComponent } from '../manage-asset.component';
 import { Asset } from '../../asset';
+import { Category } from '../../category';
+import { AssetType } from '../../asset.type';
 
 @Component({
   selector: 'app-create-asset',
@@ -30,6 +32,9 @@ export class CreateAssetComponent {
   assetService: AssetService = inject(AssetService);
   addAssetForm: FormGroup;
 
+  categoryNames: string[] = [];
+  typeNames: string[] = [];
+
   newAsset: Asset = {
     'asset_id': 1,
     'asset_name': '',
@@ -47,9 +52,65 @@ export class CreateAssetComponent {
     })
   }
 
+  ngOnInit(): void {
+    this.assetService.getCategories()
+                        .then((c: Category[]) => {
+                          c.forEach((c) => {
+                            this.categoryNames.push(c.category_name);
+                          })
+                        });
+    this.assetService.getTypes()
+                        .then((t: AssetType[]) => {
+                          t.forEach((t) => {
+                            this.typeNames.push(t.asset_type_name);
+                          })
+                        });
+  }
+
   submitAsset(): void {
     if (this.addAssetForm.valid) {
+      this.newAsset = {
+        asset_id: -1,
+        asset_name: this.addAssetForm.get('name')?.value,
+        asset_category_id: this.categoryIdByName(this.addAssetForm.get('asset_category')?.value),
+        asset_type_id: this.typeIdByName(this.addAssetForm.get('asset_type')?.value),
+        asset_url: this.addAssetForm.get('url')?.value
+      }
+      this.newAsset = this.addAssetForm.value;
+
+      this.assetService.createAsset(this.newAsset);
       console.log(this.newAsset);
+
+      this.newAsset = {
+        asset_id: 1,
+        asset_name: '',
+        asset_category_id: 1,
+        asset_type_id: 1,
+        asset_url: ''
+      }
+      this.addAssetForm = this.fb.group({
+        name: ['', Validators.required],
+        asset_category: ['', Validators.required],
+        asset_type: ['', Validators.required],
+        asset_url: ['', Validators.required]
+      })
+
     }
+  }
+
+  categoryIdByName(cat_name: string) : number {
+    let categoryId = 0;
+    this.assetService.getCategoryIdbyName(cat_name)
+                                .then((i: number) => 
+                                  {categoryId = i;})
+    return categoryId;
+  }
+
+  typeIdByName(type_name: string) : number {
+    let typeId = 0;
+    this.assetService.getCategoryIdbyName(type_name)
+                                .then((i: number) => 
+                                  {typeId = i;})
+    return typeId;
   }
 }
